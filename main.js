cb0cec7fac096e7d869ad58e765c10380a2d681b
Cham <chamboquilon1@gmail.com>
2024-11-20 16:18:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     textGeometry.center();
     const text = new THREE.Mesh(textGeometry, material);
     scene.add(text);
-    text.scale.z = 0.5;
+    text.scale.set(1, 1, 1); // Or any other desired scaling factor
 
     text.position.y = 2.75;
 });
